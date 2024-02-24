91fbd80af619d7a46a9ba6752cbc1f2aaf036361
Harrison Spain <sonspaindl@gmail.com>
2024-02-24 13:01:31 -0500

    created a for loop on the base site path to query each books data, need to get poster from api

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ const db = new pg.Client({
   password: "toor",
   port: 5432,
 })
+
+const api = "https://covers.openlibrary.org/b/isbn/"
+
 db.connect()
 
 dotenv.config()
-
 
 // const key = process.env.CRYPTO_CURRENCY_API_KEY
 
@@ -23,13 +25,21 @@ dotenv.config()
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 // declare static diectory for accessing all static files, images etc
 app.use(express.static("public"))
 
 // set default route to index.ejs
 app.get("/", async (req, res) => {
-  res.render("index.ejs")
+  const result = await db.query("Select * from items")
+  let bookList = []
+  let books = result.rows
+  books.forEach((book) => {
+    bookList.push(book)
+  })
+  console.log(bookList)
+
+  res.render("index.ejs", { books: bookList})
 })
 
 // add page
@@ -39,48 +49,44 @@ app.get("/add", async (req, res) => {
 
 // edit page
 app.get("/edit", async (req, res) => {
-  
   res.render("editBook.ejs")
 })
 
 // can just delete from the main page with action
 
+app.post("/add", async (req, res) => {
+  try {
+    const book = {
+      title: req.body["title"].toString(),
+      isbn: req.body["isbn"],
+      rating: req.body["rating"],
+      date: req.body["date"],
+      notes: req.body["notes"],
+    }
 
-
-app.post("/add",async (req,res) =>{
-  try{
-  const book = {
-    title: req.body["title"].toString(),
-    isbn: req.body["isbn"],
-    rating: req.body["rating"],
-    date: req.body["date"],
-    notes: req.body["notes"]
-  }
-
-    await db.query("INSERT INTO items (title,notes,readon,rating,isbn) VALUES ($1,$2,$3,$4,$5);",
-    [book.title,book.notes,book.date,book.rating,book.isbn])
+    await db.query(
+      "INSERT INTO items (title,notes,readon,rating,isbn) VALUES ($1,$2,$3,$4,$5);",
+      [book.title, book.notes, book.date, book.rating, book.isbn]
+    )
     const data = db.query(`SELECT * FROM items WHERE isbn = ${book.isbn}`)
-    if(data){
-    console.log(data.data)
+    if (data) {
+      console.log(data.data)
+    }
+    res.redirect("/")
+  } catch (err) {
+    console.log(err.message)
   }
-  res.redirect("/")
-
-} catch (err) {
-  console.log(err.message)
-}
-  
 })
 
-app.post("/update",(req,res) =>{
+app.post("/update", (req, res) => {
   const book = {
     title: req.body["title"].toString(),
     isbn: req.body["isbn"],
     rating: req.body["rating"],
     date: req.body["date"],
-    notes: req.body["notes"]
+    notes: req.body["notes"],
   }
 })
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
